8c98af5e8f8e911a91729b1ee7bd15cfdb0192a2
Tom Duckering <tom.duckering@gmail.com>
2017-06-05 15:14:19 +0100

    Add upload of artifacts

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ docker build --tag chef_docker_builder .
 
  # Run build in Container
 docker run --volume `pwd`:/spec_src chef_docker_builder role_webserver.spec
-DOCKER_CONTAINER_ID=$(docker ps | grep chef_docker_builder | awk '{print $1}')
+#DOCKER_CONTAINER_ID=$(docker ps | grep chef_docker_builder | awk '{print $1}')
 #docker logs $DOCKER_CONTAINER_ID
 #docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "bash -xe /htcondor-ce/tests/test_inside_docker.sh ${OS_VERSION};
 #docker ps -a
